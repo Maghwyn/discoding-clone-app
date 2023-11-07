b265dc4d2a3b720e61f588b8146290ff1f2e06d7
Mohamed <mohameddrame907@gmail.com>
2023-11-07 22:56:55 +0100

    feat/ user created in db from the form in front

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,5 +1,30 @@
+<script setup>
+
+	import SignupForm from "../components/SignupForm.vue";
+
+</script>
+
 <template>
-	<div>
-		Signup page in coming...
-	</div>
+	<SignupForm />
 </template>
+
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body{
+  width: 100vw;
+  height: 100vh;
+  background-image: url("https://wallpaperaccess.com/full/3053366.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  font-family: 'Whitney', sans-serif;
+  text-rendering: optimizeLegibility;
+  user-select: none;
+}
+
+</style>

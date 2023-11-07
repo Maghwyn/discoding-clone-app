dd2e388b7400542d78dcb1c905c1915b2c7c9d6d
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-11-07 23:45:29 +0100

    fix(views): Move the view to their respective folder

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+	<div className="flex h-screen w-full flex-col items-center justify-center">
+		<h1 className="text-2xl text-gray-400">
+			Not found
+		</h1>
+		<h2 className="text-lg text-gray-500">
+			Whooops! Couldn't find what you looking for
+		</h2>
+		<router-link
+			to="/channels/me"
+			className="py-4 text-sm text-gray-400 underline hover:text-gray-300"
+		>
+			Go back home
+		</router-link>
+	</div>
+</template>

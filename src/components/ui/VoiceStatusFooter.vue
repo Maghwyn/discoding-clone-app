90136cc575c5488d445ce3778aabaa24f76ea507
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-11-08 08:53:48 +0100

    feat(footer): Add the voice status footer

---
+++ src/components/ui/VoiceStatusFooter.vue
@@ -0,0 +1,62 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import IconMic from '@/components/icons/IconMic.vue';
+import IconHeadphones from '@/components/icons/IconHeadphones.vue';
+import IconGear from '@/components/icons/IconGear.vue';
+
+const username = ref('Maghwyn');
+const deafen = ref(false);
+const muted = ref(false);
+
+const setMute = (b: boolean) => {
+	muted.value = b;
+
+	if (!muted.value && deafen.value) {
+		deafen.value = false;
+	}
+}
+
+const setDeafen = (b: boolean) => {
+	deafen.value = b;
+	muted.value = b;
+}
+</script>
+
+<template>
+	<div className="flex justify-between gap-1 bg-semibackground px-2 py-1.5">
+		<button className="flex gap-2 rounded-md py-1 w-[120px] max-w-[120px] pl-0.5 pr-2 text-left leading-tight hover:bg-[#4E505899] overflow-hidden">
+			<img
+				src="https://picsum.photos/200?random=100"
+				alt="xxx"
+				width="32"
+				height="32"
+				class="inset-0 z-0 rounded-[50%]"
+			/>
+			<div>
+				<div className="text-xs text-white whitespace-nowrap overflow-ellipsis overflow-hidden block">{{ username }}</div>
+				<div className="text-[11px] font-light text-[#949ba4] whitespace-nowrap overflow-ellipsis overflow-hidden block">
+					Do Not Disturb
+				</div>
+			</div>
+		</button>
+		<div className="flex items-center">
+			<button
+				class="group relative flex h-8 w-8 items-center justify-center rounded-md hover:bg-[#4E505899] text-[#949ba4]"
+				@click="setMute(!muted)"
+			>
+				<IconMic width="20" height="20"/>
+				<div v-if="muted" class="absolute h-3/4 w-[5px] rotate-45 rounded-sm border-[2px] border-semibackground bg-red-500 group-hover:border-[#3a3c42]"></div>
+			</button>
+			<button
+				class="group relative flex h-8 w-8 items-center justify-center rounded-md hover:bg-[#4E505899] text-[#949ba4]"
+				@click="setDeafen(!deafen)"
+			>
+				<IconHeadphones width="20" height="20"/>
+				<div v-if="deafen" class="absolute h-3/4 w-[5px] rotate-45 rounded-sm border-[2px] border-semibackground bg-red-500 group-hover:border-[#3a3c42]"></div>
+			</button>
+			<button class="group relative flex h-8 w-8 items-center justify-center rounded-md hover:bg-[#4E505899] text-[#949ba4]">
+				<IconGear width="20" height="20"/>
+			</button>
+		</div>
+	</div>
+</template>

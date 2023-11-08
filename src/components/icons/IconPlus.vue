2fb66ac03a93a1293ccfff773c775de5a976396d
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-11-08 07:28:51 +0100

    feat(svg): Add some svg

---
+++ src/components/icons/IconPlus.vue
@@ -0,0 +1,5 @@
+<template>
+	<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+		<path d="M20 13.3333H13.3333V20H10.6667V13.3333H4V10.6667H10.6667V4H13.3333V10.6667H20V13.3333Z"/>
+	</svg>
+</template>

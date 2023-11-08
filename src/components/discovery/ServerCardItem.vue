3ea92f1f3eaa00aa64f7baca1b6c3e9409f8b1dd
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-11-08 13:44:22 +0100

    feat(discovery): Add the aside layout and the components necessary to list the public servers on discovery

---
+++ src/components/discovery/ServerCardItem.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+defineProps<{
+	image?: string;
+	name: string;
+}>()
+
+</script>
+
+<template>
+	<div class="flex flex-col overflow-hidden cursor-pointer h-[270px] flex-grow rounded-lg">
+		<div class="w-full h-[45%] object-fit">
+			<img class="w-full h-full object-cover" :src="image">
+		</div>
+		<div class="w-full h-[55%] bg-background relative">
+			<div class="absolute top-[-20px] left-[15px] w-10 h-10 min-w-[40px] bg-background rounded-xl overflow-hidden p-1">
+				<img class="w-8 h-8 object-cover rounded-lg" :src="image">
+			</div>
+			<div class="h-full w-full py-7 px-6">
+				<span class="text-white text-[14px]">{{ name }}</span>
+				<p class="text-[13px] leading-4 text-[#949ba4]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit est ab doloribus laboriosam optio tenetur</p>
+			</div>
+		</div>
+	</div>
+</template>
--- src/views/app/ServerDiscovery.vue
+++ src/views/app/ServerDiscovery.vue
@@ -1,6 +1,28 @@
 <script lang="ts" setup>
+import DiscoveryHeader from '@/components/discovery/DiscoveryHeader.vue';
+import SideDiscovery from '@/layouts/side-discovery/SideDiscovery.vue';
+import ServerCardItem from '@/components/discovery/ServerCardItem.vue';
 </script>
 
 <template>
-
+	<SideDiscovery/>
+	<div class="flex flex-grow flex-col h-full relative">
+		<div class="flex grow h-full flex-col justify-start items-center p-8 gap-12 overflow-scroll">
+			<DiscoveryHeader/>
+			<div class="discovery-grid w-full gap-3">
+				<ServerCardItem
+					v-for="(n, index) in [1,2,3,4,5,6]"
+					:image="`https://picsum.photos/720?random=${n * (index * 10)}`"
+					name="test"
+				/>
+			</div>
+		</div>
+	</div>
 </template>
+
+<style>
+.discovery-grid {
+	display: grid;
+	grid-template-columns: repeat(auto-fill,minmax(248px,1fr));
+}
+</style>

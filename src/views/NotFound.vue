731d79c558af1a8d47208f0cb305511a61b48991
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-11-08 13:42:48 +0100

    fix(class): Fix the className

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,14 +1,14 @@
 <template>
-	<div className="flex h-screen w-full flex-col items-center justify-center">
-		<h1 className="text-2xl text-gray-400">
+	<div class="flex h-screen w-full flex-col items-center justify-center">
+		<h1 class="text-2xl text-gray-400">
 			Not found
 		</h1>
-		<h2 className="text-lg text-gray-500">
+		<h2 class="text-lg text-gray-500">
 			Whooops! Couldn't find what you looking for
 		</h2>
 		<router-link
 			to="/channels/me"
-			className="py-4 text-sm text-gray-400 underline hover:text-gray-300"
+			class="py-4 text-sm text-gray-400 underline hover:text-gray-300"
 		>
 			Go back home
 		</router-link>

f21d4a7f79030a554ff8b54db18594648a29efcc
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-11-08 18:59:43 +0100

    fix(views): Rework the singup and signup view

--- src/views/auth/Signin.vue
+++ src/views/auth/Signin.vue
@@ -1,21 +1,16 @@
-<script lang="ts">
-  import { useToastStore } from '@/stores/toast.store';
-  import SignInForm from "@/components/SignInForm.vue";
-  import {defineComponent} from "vue";
+<script lang="ts" setup>
+import { useToastStore } from '@/stores/toast.store';
+import SignInForm from "@/components/form/SignInForm.vue";
 
-  export default defineComponent({
-    components: {SignInForm}
-  })
-
-  const alert = useToastStore();
-  if (alert.validationFromSignup) {
-    setTimeout(function() {
-      alert.showSuccessToast(alert.messageContent)
-      alert.validationFromSignup = false
-    }, 500)
-  }
-	
+const alert = useToastStore();
+if (alert.validationFromSignup) {
+	setTimeout(function() {
+		alert.showSuccessToast(alert.messageContent)
+		alert.validationFromSignup = false
+	}, 500)
+}
 </script>
+
 <template>
 	<div class="container-signin-form">
 		<SignInForm />
@@ -23,14 +18,14 @@
 </template>
 
 <style>
-  .container-signin-form {
-    width: 100vw;
-    height: 100vh;
-    background-image: url("https://wallpaperaccess.com/full/3053366.png");
-    background-size: cover;
-    background-repeat: no-repeat;
-    font-family: 'Whitney', sans-serif;
-    text-rendering: optimizeLegibility;
-    user-select: none;
-  }
+.container-signin-form {
+	width: 100vw;
+	height: 100vh;
+	background-image: url("/discord-background.png");
+	background-size: cover;
+	background-repeat: no-repeat;
+	font-family: 'Whitney', sans-serif;
+	text-rendering: optimizeLegibility;
+	user-select: none;
+}
 </style>

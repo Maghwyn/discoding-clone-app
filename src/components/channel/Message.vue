e1d4a9c0a31bee3ad3a62ce64796c597340c125a
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-11-09 07:58:55 +0100

    feat(message): Add a ui styling to a blocked message

--- src/components/channel/Message.vue
+++ src/components/channel/Message.vue
@@ -5,6 +5,7 @@ import IconDelete from '@/components/icons/IconDelete.vue';
 import IconEdit from '@/components/icons/IconEdit.vue';
 import IconBlock from '@/components/icons/IconBlock.vue';
 import IconFlag from '@/components/icons/IconFlag.vue';
+import IconCross from '@/components/icons/IconCross.vue';
 import { formatDateHM, formatDateYMDHM } from '@/utils/date.helper';
 import { editMessage } from '@/api/messages.req';
 import { deletePrivateMessage } from '@/api/messages.req';
@@ -116,9 +117,13 @@ const signalUser = () => {
 </script>
 
 <template>
-	<div class="w-full px-4 message-wrapper" :class="{ 'mt-3': !isFollowup }">
+	<div class="w-full px-4 message-wrapper" :class="{ 'mt-3': !isFollowup && !isBlocked }">
 		<div class="w-full flex py-0.5 gap-3 justify-center transition duration-200 relative message-container">
-			<div v-if="!isEditMode" class="message-menu absolute hidden justify-center items-center cursor-pointer right-[10px] top-[-10px] text-[14px] rounded-md bg-[#2e3035] border border-[#26282c] text-[#949ba4]">
+			<div
+				v-if="!isEditMode"
+				class="message-menu absolute hidden justify-center items-center cursor-pointer right-[10px] text-[14px] rounded-md bg-[#2e3035] border border-[#26282c] text-[#949ba4]"
+				:class="{ 'top-[-10px]': !isFollowup, 'top-[-22.5px]': isFollowup }"
+			>
 				<div v-if="canEdit" class="px-1.5 py-1 flex items-center justify-center">
 					<IconEdit @click="openEditMode" width="18" height="18" class="hover:text-orange-500"/>
 				</div>
@@ -133,44 +138,52 @@ const signalUser = () => {
 					<IconFlag @click="signalUser" width="18" height="18" class="hover:text-red-500"/>
 				</div>
 			</div>
-			<template v-if="!isFollowup">
-				<div v-if="userPicture" class="flex items-center justify-center w-8 h-8 min-w-[32px] rounded-[50%]">
-					<img
-						:src="userPicture"
-						alt="xxx"
-						width="32"
-						height="32"
-						class="inset-0 z-0 rounded-[50%]"
-					/>
+			<template v-if="!isBlocked">
+				<template v-if="!isFollowup">
+					<div v-if="userPicture" class="flex items-center justify-center w-8 h-8 min-w-[32px] rounded-[50%]">
+						<img
+							:src="userPicture"
+							alt="xxx"
+							width="32"
+							height="32"
+							class="inset-0 z-0 rounded-[50%]"
+						/>
+					</div>
+					<div v-else class="flex items-center justify-center w-8 h-8 min-w-[32px] rounded-[50%] bg-pink-400 text-white">
+						<IconDiscord width="20" height="20"/>
+					</div>
+				</template>
+				<div v-else class="flex items-center justify-center text-[7px] text-[#949ba4] leading-none w-8 min-w-[32px] message-sm-date">
+					<span class="hidden">{{ formatDateHM(new Date(createdAt)) }}</span>
 				</div>
-				<div v-else class="flex items-center justify-center w-8 h-8 min-w-[32px] rounded-[50%] bg-pink-400 text-white">
-					<IconDiscord width="20" height="20"/>
-				</div>
-			</template>
-			<div v-else class="flex items-center justify-center text-[7px] text-[#949ba4] leading-none w-8 min-w-[32px] message-sm-date">
-				<span class="hidden">{{ formatDateHM(new Date(createdAt)) }}</span>
-			</div>
-			<div class="flex flex-col grow gap-0.5">
-				<div v-if="!isFollowup" class="flex gap-2 items-end">
-					<span class="leading-none text-[13px] font-bold text-white">{{ username }}</span>
-					<span class="text-[10px] text-[#949ba4] leading-none">{{ formatDateYMDHM(new Date(createdAt)) }}</span>
-				</div>
-				<div v-if="!isEditMode" class="text-[12px] text-white font-light">
-					{{ content }}
-				</div>
-				<div v-show="isEditMode" class="my-1 flex flex-col">
-					<textarea
-						id="edit-message"
-						class="bg-[#383a3f] p-3 select-none border-none outline-none resize-none w-full grow text-xs max-h-[45vh] h-8 block text-white rounded-[8px]"
-						ref="temporaryInput"
-						@input="onInput"
-						@keydown="listenKeyPress"
-					></textarea>
-					<div>
-						<span class="text-white font-light text-[10px]">escape to <span class="text-red-400 hover:underline cursor-pointer" @click="closeEditMode">cancel</span> • </span>
-						<span class="text-white font-light text-[10px]">enter to <span class="text-blue-400 hover:underline cursor-pointer" @click="confirmEditMessage">save</span></span>
+				<div class="flex flex-col grow gap-0.5">
+					<div v-if="!isFollowup" class="flex gap-2 items-end">
+						<span class="leading-none text-[13px] font-bold text-white">{{ username }}</span>
+						<span class="text-[10px] text-[#949ba4] leading-none">{{ formatDateYMDHM(new Date(createdAt)) }}</span>
+					</div>
+					<div v-if="!isEditMode" class="text-[12px] text-white font-light">
+						{{ content }}
+					</div>
+					<div v-show="isEditMode" class="my-1 flex flex-col">
+						<textarea
+							id="edit-message"
+							class="bg-[#383a3f] p-3 select-none border-none outline-none resize-none w-full grow text-xs max-h-[45vh] h-8 block text-white rounded-[8px]"
+							ref="temporaryInput"
+							@input="onInput"
+							@keydown="listenKeyPress"
+						></textarea>
+						<div>
+							<span class="text-white font-light text-[10px]">escape to <span class="text-red-400 hover:underline cursor-pointer" @click="closeEditMode">cancel</span> • </span>
+							<span class="text-white font-light text-[10px]">enter to <span class="text-blue-400 hover:underline cursor-pointer" @click="confirmEditMessage">save</span></span>
+						</div>
 					</div>
 				</div>
+			</template>
+			<div v-else class="flex w-full py-2 gap-4">
+				<div class="flex items-center justify-center w-8 min-w-[32px]">
+					<IconCross width="14" height="14" class="text-[#949ba4]"/>
+				</div>
+				<span class="text-[#949ba4] text-[12px]">Blocked message</span>
 			</div>
 		</div>
 	</div>

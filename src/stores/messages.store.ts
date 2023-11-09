a8e81938c11df8c4e99ba7d51ab43f2297fb2d4d
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-11-09 10:43:57 +0100

    feat(store): Update the messages store and create the direct messages store with their own types

--- src/stores/messages.store.ts
+++ src/stores/messages.store.ts
@@ -2,13 +2,14 @@ import { defineStore } from 'pinia';
 
 import { pick } from '@/utils/object.helper';
 import type { KeysRequired } from '@/interfaces/advanced-types.interface';
-import type { MessagesStore } from '@/interfaces/message.interface';
+import type { MessagesStore } from '@/interfaces/messages.interface';
 
 const messagesStoreDefaultState = (): MessagesStore => ({
 	lastEditId: null,
+	messages: []
 });
 
-export const useMessagesStore = defineStore('auth', {
+export const useMessagesStore = defineStore('messages', {
 	state: (): MessagesStore => messagesStoreDefaultState(),
 	actions: {
 		reset(this: MessagesStore, keys?: Array<KeysRequired<MessagesStore>>) {

f267ebfd1e615874392f98fea24b7c64fbf52bda
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-11-09 10:41:24 +0100

    feat(channel): Update the channel component to inject the necessary data

--- src/components/channel/MessageInput.vue
+++ src/components/channel/MessageInput.vue
@@ -6,10 +6,9 @@ import { sendPrivateMessage } from '@/api/messages.req';
 const props = defineProps<{
 	context: 'server' | 'conversation',
 	contextId: string;
+	channelName: string;
+	isBlocked: boolean;
 }>();
-
-// TODO: If the user is blocked when the context is "conversation", modify the ui as in "you cannot send a message to this user"
-// TODO: For that we need to retrieve the relationship tho.
 
 const sendAction = props.context === 'conversation' ? sendPrivateMessage : () => {};
 const inputArea = ref<HTMLTextAreaElement>();
@@ -44,7 +43,7 @@ const sendMessage = async (event: KeyboardEvent) => {
 	<div class="px-4 mt-[-8px] bottom-0 w-full">
 		<div class="relative w-full text-left rounded-[8px] mb-[24px]">
 			<div class="overflow-x-hidden overflow-y-scroll max-h-1/2 rounded-[8px] backface-hidden">
-				<label for="send-message" class="w-full min-h-[44px] max-h-[50vh] px-4 bg-gray-900 flex">
+				<label for="send-message" class="w-full min-h-[44px] max-h-[50vh] px-4 bg-gray-900 flex" :class="{ 'grayscale': isBlocked }">
 					<div class="h-auto flex items-start justify-center">
 						<div class="h-full pr-3 py-3 text-[#949ba4]">
 							<IconRoundPlus/>
@@ -52,13 +51,17 @@ const sendMessage = async (event: KeyboardEvent) => {
 					</div>
 					<div class="flex grow pt-[13px] pb-[12px]">
 						<textarea
+							v-if="!isBlocked"
 							id="send-message"
 							class="bg-transparent select-none border-none outline-none resize-none w-full grow text-xs max-h-[45vh] block h-4 text-white"
-							placeholder="Message #RandomGuy"
+							:placeholder="`Message ${context === 'conversation' ? '@' : '#' }${channelName}`"
 							ref="inputArea"
 							@input="onInput"
 							@keydown="sendMessage"
 						></textarea>
+						<span v-else class="bg-transparent select-none border-none outline-none resize-none w-full grow text-xs max-h-[45vh] block text-[#949ba4]">
+							You cannot send a message in this channel
+						</span>
 					</div>
 					<div class="h-auto flex items-start justify-center">
 						<div class="w-10 max-w-[40px] py-3 pl-3 flex justify-center items-center text-[#949ba4]">
--- src/components/channel/MessageOrigin.vue
+++ src/components/channel/MessageOrigin.vue
@@ -1,14 +1,31 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
+import IconDiscord from '@/components/icons/IconDiscord.vue';
 
-const name = ref("name");
+defineProps<{
+	userPicture?: string;
+	channelName: string;
+	type: 'conversation' | 'server'
+}>();
 </script>
 
 
 <template>
 	<div class="w-full h-fit flex flex-col px-[1rem] py-[1rem] gap-4">
-		<span class="font-bold text-[20px] text-white">{{ name }}</span>
-		<span class="font-light text-[14px] fw-normal text-[#949ba4]">This is the beginning of your direct message history with <b>{{ name }}</b></span>
+		<div v-if="type === 'conversation'" class="flex items-center w-12 h-12 mr-1 gap-4">
+			<img
+				v-if="userPicture"
+				:src="userPicture"
+				alt="xxx"
+				width="40"
+				height="40"
+				class="inset-0 z-0 rounded-[50%]"
+			/>
+			<div v-else class="flex items-center justify-center w-12 h-12 min-w-[48px] rounded-[50%] bg-pink-400 text-white">
+				<IconDiscord width="32" height="32"/>
+			</div>
+			<span class="font-bold text-[20px] text-white">{{ channelName || 'unknown' }}</span>
+		</div>
+		<span class="font-light text-[14px] fw-normal text-[#949ba4]">This is the beginning of your direct message history with <b>{{ channelName || 'unknown' }}</b></span>
 		<div class="w-full h-[1px] bg-gray-800"/>
 	</div>
 </template>

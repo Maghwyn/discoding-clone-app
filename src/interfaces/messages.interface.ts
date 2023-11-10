120f12ffc27f80a9a516e864fcd9f539e743bcff
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-11-10 10:19:24 +0100

    feat(message-store): Update the messages store for the unreads

--- src/interfaces/messages.interface.ts
+++ src/interfaces/messages.interface.ts
@@ -3,4 +3,13 @@ import type { Message } from "@/api/messages.req.type";
 export interface MessagesStore {
 	lastEditId: string;
 	messages: Array<Message>
+	unreads: Array<Message>;
+}
+
+export type UnreadMessageGroup = {
+	id: string;
+	username: string;
+	userPicture: string;
+	channelId: string;
+	notificationCount: number;
 }
